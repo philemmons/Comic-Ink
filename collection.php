5efe76865f4862f631b6561b68d1582bce805be6
philemmons <pemmons@csumb.edu>
2023-10-25 13:22:45 -0700

    add comment to collection.php update login.php nav

--- collection.php
+++ collection.php
@@ -66,6 +66,7 @@ function dataDisplay($comic)
 }
 ?>
 
+<!-- Collect the nav links, forms, and other content for toggling -->
 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
     <li class="nav-item">
         <a class="nav-link" href="index.php">Home</a>
