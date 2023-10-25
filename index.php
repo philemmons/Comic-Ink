e2b45af54b11e20adf74152126326970e6f12bd1
philemmons <pemmons@csumb.edu>
2023-10-24 17:37:56 -0700

    index navbar classes changed

--- index.php
+++ index.php
@@ -7,9 +7,9 @@ if (isset($_POST['logout'])) {
   session_destroy();
   header("Location: index.php");
 }
-
 ?>
-<!-- Collect the nav links, forms, and other content for toggling -->
+
+<!-- Collect the nav links, forms, and other content for toggling 
 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
   <ul class="nav navbar-nav">
     <li class="active"><a href="index.php">Home<span class="sr-only">(current)</span></a></li>
@@ -17,17 +17,40 @@ if (isset($_POST['logout'])) {
     <li><a href="convention.php">Convention</a></li>
     <li><a href="login.php">Admin</a></li>
   </ul>
+-->
 
-  <?php
-  if (isset($_SESSION["username"])) {
-    echo '<form method ="POST" id="one" >';
-    echo '<input type="submit" value="Logout" class="btn" name="logout" style="box-shadow: none !important; margin-top: 4px;"/>';
-    echo '</form>';
-  }
-  ?>
+<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" hhref="collection.php">Collection</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="convention.php">Convention</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="login.php">Admin</a>
+  </li>
+</ul>
 
-</div><!-- /.navbar-collapse -->
-</div><!-- /.container-fluid -->
+<!--
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+-->
+
+<?php
+if (isset($_SESSION["username"])) {
+  echo '<form method ="POST" id="one" >';
+  echo '<input type="submit" value="Logout" class="btn" name="logout" style="box-shadow: none !important; margin-top: 4px;"/>';
+  echo '</form>';
+}
+?>
+
+</div><!-- .collapse navbar-collapse -->
+</div><!-- .container-fluid -->
 </nav>
 
 <div id="carouselCointainer">
