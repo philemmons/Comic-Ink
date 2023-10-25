3ba33b932047d9c2eaf1675f7d836dd68a242d96
philemmons <pemmons@csumb.edu>
2023-10-25 09:02:51 -0700

    test fade carousel

--- index.php
+++ index.php
@@ -42,13 +42,13 @@ if (isset($_SESSION["username"])) {
 <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="img/1.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="img/2.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="img/3.jpg" class="d-block w-100" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
@@ -61,160 +61,7 @@ if (isset($_SESSION["username"])) {
   </button>
 </div>
 
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
 
-
-<div id="carouselCointainer">
-  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="5"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="6"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="7"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="8"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="9"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="10"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="11"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-      <div class="item active">
-        <img src="img/1.jpg" alt="">
-        <div class="carousel-caption">
-          ...
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/2.jpg" alt="">
-        <div class="carousel-caption">
-          ...
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/3.jpg" alt="">
-        <div class="carousel-caption">
-          ...
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/4.jpg" alt="">
-        <div class="carousel-caption">
-          ...
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/5.jpg" alt="">
-        <div class="carousel-caption">
-          ...
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/6.png" alt="">
-        <div class="carousel-caption">
-          ...
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/4.jpg" alt="">
-        <div class="carousel-caption">
-          ...
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/7.jpg" alt="">
-        <div class="carousel-caption">
-          ...
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/8.jpg" alt="">
-        <div class="carousel-caption">
-          ...
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/9.jpg" alt="">
-        <div class="carousel-caption">
-          ...
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="img/10.jpg" alt="">
-        <div class="carousel-caption">
-          ...
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/11.jpg" alt="">
-        <div class="carousel-caption">
-          ...
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/12.jpg" alt="">
-        <div class="carousel-caption">
-          ...
-        </div>
-      </div>
-
-    </div><!-- end wrapper -->
-
-    <!-- Controls -->
-    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>
-<!-- //carousel -->
 </body>
 
 </html>
