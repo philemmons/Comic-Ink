f0107bc21bc84b6d82e696438269257297cb6612
philemmons <pemmons@csumb.edu>
2023-10-24 17:55:10 -0700

    examine js tooltip function; removed navbar-brand class

--- index.php
+++ index.php
@@ -35,11 +35,11 @@ if (isset($_POST['logout'])) {
 </ul>
 
 <!--
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
--->
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+    -->
 
 <?php
 if (isset($_SESSION["username"])) {
