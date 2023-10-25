ba6932ee04dc07ec06eefd70c76a249286692896
philemmons <pemmons@csumb.edu>
2023-10-25 11:41:48 -0700

    test individual carousel interval

--- index.php
+++ index.php
@@ -39,23 +39,23 @@ if (isset($_SESSION["username"])) {
 
 <!-- https://getbootstrap.com/docs/5.0/components/carousel/ -->
 
-<div id="carouselExampleFade center-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="img/1.jpg" class="d-block w-100" alt="...">
+    <div class="carousel-item active" data-bs-interval="2000">
+      <img src="img/1.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="img/3.png" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="img/2.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="img/3.jpg" class="d-block w-100" alt="...">
+      <img src="img/3.png" class="d-block w-100" alt="...">
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
