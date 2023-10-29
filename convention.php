b105baffaa3ff5711b39c1493ed81d9b67a54f3f
philemmons <pemmons@csumb.edu>
2023-10-29 08:28:06 -0700

    col-sm-2 to col-auto

--- convention.php
+++ convention.php
@@ -58,25 +58,25 @@ if (isset($_SESSION["status"])) {
 <br>
 <div class="wrapper form-display">
     <form method="POST" name="conForm" class="row gx-3 gy-2 align-items-center">
-        <div class="col-sm-2">
+        <div class="col-auto">
             Welcome <?= $_SESSION['name'] ?>
         </div>
-        <div class="col-sm-2">
+        <div class="col-auto">
             <input type="submit" value="Search" name="filterForm" class="btn" />
         </div>
-        <div class="col-sm-2">
+        <div class="col-auto">
             <input type="submit" value="All Conventions" name="allIn" class="btn" />
         </div>
 
-        <div class="col-sm-2">
+        <div class="col-auto">
             <label id="l5">Name:</label>
             <input type="text" name="conName" size="20" placeholder="enter convention name here." />
         </div>
-        <div class="col-sm-2">
+        <div class="col-auto">
             <label id="l6">Creator:</label>
             <input type="text" name="creator" size="20" placeholder="first or last name" />
         </div>
-        <div class="col-sm-2">
+        <div class="col-auto">
             <label id="l7">State:</label>
             <select name="state">
                 <option value="" disabled selected>select one</option>
@@ -89,7 +89,7 @@ if (isset($_SESSION["status"])) {
                 ?>
             </select>
         </div>
-        <div class="col-sm-2">
+        <div class="col-auto">
             <label id="l8">Sort By:</label>
             <select name="sortBy">
                 <option value="" disabled selected>select one</option>
