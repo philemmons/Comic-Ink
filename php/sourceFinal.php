9a601ade6f8f183598df5885952065d02a9e9a36
philemmons <philemmons.now@gmail.com>
2023-11-06 12:38:08 -0800

    examine textdate

--- php/sourceFinal.php
+++ php/sourceFinal.php
@@ -165,8 +165,8 @@ function goSQLcon($table)
         //Convert date to text
 
 echo $conDate. "<br>";
-        $textDate = $conDate->format("m/d");
-
+        $textDate = $conDate->format("Y-m-d");
+echo $textDate. "<br>";
 
         //Prevents SQL injection by using a named parameter.
         $nPara[':dConDate'] = '%' . $textDate . '%';
