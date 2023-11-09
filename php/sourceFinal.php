c9bf5d37ab9cc41aecbe2034185b1f250cde85af
philemmons <philemmons.now@gmail.com>
2023-11-09 15:57:35 -0800

    test4

--- php/sourceFinal.php
+++ php/sourceFinal.php
@@ -240,8 +240,6 @@ function getConInfo($conID)
 {
     global $dbConn, $nPara;
 
-    echo $conID. "<br>";die();
-
     $nPara[':dConId'] = $conID;
     $sql = "SELECT * FROM convention WHERE id = :dConId ";
     $stmt = $dbConn->prepare($sql);
