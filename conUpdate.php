6a98bdabd03ac787babc2599727291e82810d1aa
philemmons <philemmons.now@gmail.com>
2023-11-09 16:19:07 -0800

    conditional with a selected convention

--- conUpdate.php
+++ conUpdate.php
@@ -38,7 +38,7 @@ if (isset($_POST['submitUpdate'])) {  //admin has submitted the "update user" fo
   $nPara[':city'] = htmlspecialchars($_POST['city'], ENT_QUOTES);
   $nPara[':state'] = htmlspecialchars($_POST['state'], ENT_QUOTES);
   $nPara[':country'] = htmlspecialchars($_POST['country'], ENT_QUOTES);
-  $nPara[':website'] = htmlspecialchars(preg_replace("(^https?://)", "",$_POST['website']), ENT_QUOTES);
+  $nPara[':website'] = htmlspecialchars(preg_replace("(^https?://)", "", $_POST['website']), ENT_QUOTES);
 
   $stmt = $dbConn->prepare($sql);
   $stmt->execute($nPara);
@@ -48,11 +48,6 @@ if (isset($_POST['submitUpdate'])) {  //admin has submitted the "update user" fo
   sleep(2); // pause the modal
 
 } //eof if
-
-if (isset($_GET['id'])) {
-  //echo $_GET['id']; die();
-  $conInfo = getConInfo($_GET['id']);
-}
 
 ?>
 
@@ -106,101 +101,109 @@ if (isset($_SESSION["status"])) {
     Welcome <?= $_SESSION['name'] ?> - Update Convention Info
   </h6>
   <br>
-  <form method='POST' name= "updateConForm" class='row gx-4 gy-3 align-items-center' onsubmit='validateUpdate()';>
+  
+  <?php
+  if (isset($_GET['id'])) {
+    $conInfo = getConInfo($_GET['id']);
+  ?>
+    <form method='POST' name="updateConForm" class='row gx-4 gy-3 align-items-center' onsubmit='validateUpdate()' ;>
 
-    <div class="col-md-2">
-      <div class="form-floating">
-        <input type="text" class="form-control" id="conID" placeholder="Default - auto incremented" name="conID" value="<?= $conInfo['id'] ?>" disabled/>
-        <label for="conID">ConID</label>
-        <span id="conIDError"></span>
+      <div class="col-md-2">
+        <div class="form-floating">
+          <input type="text" class="form-control" id="conID" placeholder="Default - auto incremented" name="conID" value="<?= $conInfo['id'] ?>" disabled />
+          <label for="conID">ConID</label>
+          <span id="conIDError"></span>
+        </div>
       </div>
-    </div>
 
-    <div class="col-md-10">
-      <div class="form-floating">
-        <input type="text" class="form-control" id="conName" placeholder="Enter Convention Name" name="conName" value="<?= $conInfo['conName'] ?>" />
-        <label for="conName">Convention Name</label>
-        <span id="conNameError"></span>
+      <div class="col-md-10">
+        <div class="form-floating">
+          <input type="text" class="form-control" id="conName" placeholder="Enter Convention Name" name="conName" value="<?= $conInfo['conName'] ?>" />
+          <label for="conName">Convention Name</label>
+          <span id="conNameError"></span>
+        </div>
       </div>
-    </div>
 
-    <div class="col-md-4">
-      <div class="form-floating">
-        <input type="text" class="form-control" id="start_date" name="start_date" value="<?= $conInfo['start_date'] ?>" />
-        <label for="start_date">Start Date(Month and Day only)</label>
-        <span id="start_dateError"></span>
+      <div class="col-md-4">
+        <div class="form-floating">
+          <input type="text" class="form-control" id="start_date" name="start_date" value="<?= $conInfo['start_date'] ?>" />
+          <label for="start_date">Start Date(Month and Day only)</label>
+          <span id="start_dateError"></span>
+        </div>
       </div>
-    </div>
 
-    <div class="col-md-4">
-      <div class="form-floating">
-        <input type="text" class="form-control" id="end_date" name="end_date" value="<?= $conInfo['end_date'] ?>" />
-        <label for="end_date">End Date(Month and Day only)</label>
-        <span id="end_dateError"></span>
+      <div class="col-md-4">
+        <div class="form-floating">
+          <input type="text" class="form-control" id="end_date" name="end_date" value="<?= $conInfo['end_date'] ?>" />
+          <label for="end_date">End Date(Month and Day only)</label>
+          <span id="end_dateError"></span>
+        </div>
       </div>
-    </div>
 
-    <div class="col-md-4">
-      <div class="form-floating">
-        <input type="int" class="form-control" id="year" placeholder="Enter Year:" name="year" value="<?= $conInfo['year'] ?>" />
-        <label for="year">Year</label>
-        <span id="yearError"></span>
+      <div class="col-md-4">
+        <div class="form-floating">
+          <input type="int" class="form-control" id="year" placeholder="Enter Year:" name="year" value="<?= $conInfo['year'] ?>" />
+          <label for="year">Year</label>
+          <span id="yearError"></span>
+        </div>
       </div>
-    </div>
 
-    <div class="col-12">
-      <div class="form-floating">
-        <input type="text" class="form-control" id="event_location" placeholder="Enter Location" name="event_location" value="<?= $conInfo['event_location'] ?>" />
-        <label for="event_location">Event Location</label>
-        <span id="event_locationError"></span>
+      <div class="col-12">
+        <div class="form-floating">
+          <input type="text" class="form-control" id="event_location" placeholder="Enter Location" name="event_location" value="<?= $conInfo['event_location'] ?>" />
+          <label for="event_location">Event Location</label>
+          <span id="event_locationError"></span>
+        </div>
       </div>
-    </div>
 
-    <div class="col-md-4">
-      <div class="form-floating">
-        <input type="text" class="form-control" id="city" placeholder="Enter City" name="city" value="<?= $conInfo['city'] ?>" />
-        <label for="city">City</label>
-        <span id="cityError"></span>
+      <div class="col-md-4">
+        <div class="form-floating">
+          <input type="text" class="form-control" id="city" placeholder="Enter City" name="city" value="<?= $conInfo['city'] ?>" />
+          <label for="city">City</label>
+          <span id="cityError"></span>
+        </div>
       </div>
-    </div>
 
-    <div class="col-md-4">
-      <div class="form-floating">
-        <input type="text" class="form-control" id="state" placeholder="Enter State" name="state" value="<?= $conInfo['state'] ?>" />
-        <label for="state">State</label>
-        <span id="stateError"></span>
+      <div class="col-md-4">
+        <div class="form-floating">
+          <input type="text" class="form-control" id="state" placeholder="Enter State" name="state" value="<?= $conInfo['state'] ?>" />
+          <label for="state">State</label>
+          <span id="stateError"></span>
+        </div>
       </div>
-    </div>
 
-    <div class="col-md-4">
-      <div class="form-floating">
-        <input type="text" class="form-control" id="country" placeholder="Enter Country" name="country" value="<?= $conInfo['country'] ?>" />
-        <label for="country">Country</label>
-        <span id="countryError"></span>
+      <div class="col-md-4">
+        <div class="form-floating">
+          <input type="text" class="form-control" id="country" placeholder="Enter Country" name="country" value="<?= $conInfo['country'] ?>" />
+          <label for="country">Country</label>
+          <span id="countryError"></span>
+        </div>
       </div>
-    </div>
 
-    <div class="col-md-12">
-      <div class="form-floating">
-        <input type="text" class="form-control" id="website" placeholder="xxx.example.xxx" name="website" value="<?= $conInfo['website'] ?>" />
-        <label for="website">Website</label>
-        <span id="websiteError"></span>
+      <div class="col-md-12">
+        <div class="form-floating">
+          <input type="text" class="form-control" id="website" placeholder="xxx.example.xxx" name="website" value="<?= $conInfo['website'] ?>" />
+          <label for="website">Website</label>
+          <span id="websiteError"></span>
+        </div>
       </div>
-    </div>
 
-    <div class="col-md-4">
-      <input type="submit" name="submitUpdate" value="Update" class="btn" />
-    </div>
+      <div class="col-md-4">
+        <input type="submit" name="submitUpdate" value="Update" class="btn" />
+      </div>
 
-    <div class="col-md-4">
-      <button type="reset" name="reset" value="reset" class="btn"> Reset </button>
-    </div>
+      <div class="col-md-4">
+        <button type="reset" name="reset" value="reset" class="btn"> Reset </button>
+      </div>
+    <?php } else {  ?>
+      <h6> Hello, there was no convention selected which to update, and please select one from the Admin panel.</h6>
+    <?php } ?>
 
     <div class="col-md-4">
       <a href="admin.php" class="btn" data-dismiss="modal" style="float:right">Return to Admin</a>
     </div>
 
-  </form>
+    </form>
 </div>
 
 <div id="modal-content" class="modal fade" tabindex="-1" role="dialog">
