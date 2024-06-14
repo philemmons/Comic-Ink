1f00fb4acc484ca48a2a7fb6addea6e85b90cb46
philemmons <pemmons@csumb.edu>
2024-06-14 13:51:01 -0700

    css; layout

--- index.php
+++ index.php
@@ -89,12 +89,12 @@ if (isset($_SESSION["status"])) {
     </div>
 
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="carousel-control-prev-icon" aria-hidden="true" title="Previous"></span>
       <span class="visually-hidden">Previous</span>
     </button>
 
     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="carousel-control-next-icon" aria-hidden="true" title="Next"></span>
       <span class="visually-hidden">Next</span>
     </button>
 
