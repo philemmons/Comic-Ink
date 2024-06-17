01cbe5323a22011f45bea26ebfc80b8744e80303
philemmons <pemmons@csumb.edu>
2024-06-17 12:00:49 -0700

    layout

--- index.php
+++ index.php
@@ -117,8 +117,8 @@ if (isset($_SESSION["status"])) {
 
   <article aria-label="Important because">
     <div class="container mt-5">
-      <div class="row justify-content-center">
-        <div class="col-xl-12 p-3 bg-body-tertiary  text-white">
+      <div class="row justify-content-center mb-5">
+        <div class="col-xl-12 p-3 bg-body-tertiary text-white mb-5">
           <p>These comics are significant not only for their storytelling and artistic achievements but also for their impact on the comic book industry and popular culture. They have inspired countless adaptations in film, television, and other media, and continue to influence new generations of creators and readers.</p>
         </div>
       </div>
