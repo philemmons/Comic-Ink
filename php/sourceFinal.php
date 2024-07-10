73ed6fc04ac2905efeb713182f1736bfb7aec265
philemmons <pemmons@csumb.edu>
2024-07-09 17:33:10 -0700

    test

--- php/sourceFinal.php
+++ php/sourceFinal.php
@@ -233,12 +233,12 @@ function goMain()
         echo "<br><span style='color:red'><h4>Wrong username or password.</h4></span>";
         echo "</form>";
     } else {
-        $_SESSION["name"] = $record['firstName'] . " " . $record['lastName'];
-        $_SESSION["username"]  = $record['userName'];
-        $_SESSION["status"] = getenv('LOGIN_STATUS');
+       // $_SESSION["name"] = $record['firstName'] . " " . $record['lastName'];
+       // $_SESSION["username"]  = $record['userName'];
+       // $_SESSION["status"] = getenv('LOGIN_STATUS');
         //echo $_SESSION["status"];
         header("Location: admin.php");
-        exit; //redirect to home page
+        exit; //redirect to admin page
     }
 }
 
